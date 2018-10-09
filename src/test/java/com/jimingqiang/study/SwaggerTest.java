173f16f9d2d9b85377e4cc1c7fc5960d7c0bca0c
jimingqiang <jimingqiang@qd.me>
2018-10-09 18:09:35 +0800

    config test

---
+++ src/test/java/com/jimingqiang/study/SwaggerTest.java
@@ -0,0 +1,15 @@
+package com.jimingqiang.study;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by QDHL on 2018/10/9.
+ *
+ * @author mingqiang ji
+ */
+@SpringBootTest(classes=StudyApplication.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SwaggerTest {
+}
